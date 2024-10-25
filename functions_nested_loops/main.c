1bb5d85499e535ad93c4f6edf347ab128bd607dc
ginftls <scdr1905@gmail.com>
2024-10-25 04:13:21 -0400

    Ofuncnested

---
+++ functions_nested_loops/main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char *str = "_putchar\n";
+
+    while (*str)
+    {
+        _putchar(*str);
+        str++;
+    }
+
+    return (0);
+}
