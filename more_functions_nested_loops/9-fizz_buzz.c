c20171c609653764cb2d5a65d32583f015ccc08e
ginftls <scdr1905@gmail.com>
2024-10-29 14:06:30 -0400

    09

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,19 +1,40 @@
-#include <unistd.h>
+#include "main.h"
+#include <stdio.h>
 
-void print_fizz_buzz(int n) {
-	if (n % 15 == 0)
-		write("FizzBuzz\n", 10);
-	else if (n % 5 == 0)
-		write("Buzz\n", 6);
-	else if (n % 3 == 0)
-		write("Fizz\n", 5);
-	else
-		printf("%d\n", n);
-}
-
-int main() {
+/**
+ * main - prints numbers 1-100 followed by a new line
+ * for multiples of 3, prints Fizz
+ * for multiples of 5, prints Buzz
+ * for multiples of both 3 and 5, prints FizzBuzz
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 	int i;
+
 	for (i = 1; i <= 100; i++)
-		print_fizz_buzz(i);
-	return 0;
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+
+		if (i < 100)
+			printf(" ");
+	}
+	printf("\n");
+
+	return (0);
 }
