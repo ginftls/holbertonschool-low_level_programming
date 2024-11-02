a7f94765b362ec8465ec01fb407256406ae71f47
ginftls <scdr1905@gmail.com>
2024-11-02 09:51:35 -0400

    00

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* _strcat - Concatenates two strings
+* @dest: The destination string
+* @src: The source string to append
+*
+* Return: A pointer to the resulting string dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int dest_len = 0;
+	int i = 0;
+
+	/* Find the length of dest string */
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+
+	/* Copy each character from src to dest */
+	while (src[i] != '\0')
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+
+	/* Add terminating null byte */
+	dest[dest_len + i] = '\0';
+
+	return (dest);
+}
