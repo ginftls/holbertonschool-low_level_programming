9122e20b2e3f676aeb416ca74e44c81a5f5cfd9b
ginftls <scdr1905@gmail.com>
2024-11-04 13:07:14 -0500

    04

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,6 +1,14 @@
 #include "main.h"
 #include <stddef.h> /* for NULL definition */
 
+/**
+ * _strchr Write a function that locates a character in a string.
+ * @s: The string to be scanned
+ * @accept: The string containing the characters to match
+ *
+ * Return: Number of bytes in initial segment of s consisting of accept bytes
+ */
+
 char *_strchr(char *s, char c) /* _strchr returns a pointer first occurrence */
 {
 	char *f = NULL;
