cdc81aa20cd47b8ed87dada66154a2f249302922
ginftls <scdr1905@gmail.com>
2024-11-13 06:09:23 -0500

    00

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 
 /**
- * malloc_checked - Allocates memory using malloc.
- * @b: The number of bytes to allocate.
+ *malloc_checked - Allocates memory using malloc.
+ *@b: The number of bytes to allocate.
  *
- * Return: A pointer to the allocated memory.
- * If malloc fails, the function terminates the process with a status value of 98.
+ *Return: A pointer to the allocated memory.
+ *If malloc fails,the function terminates the processwitha status value of 98.
  */
 void *malloc_checked(unsigned int b)
 {
