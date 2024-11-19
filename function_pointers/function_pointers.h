895a16d983528ede1f3d604ae965ff0c7d259ee1
ginftls <scdr1905@gmail.com>
2024-11-19 05:33:52 -0500

    o2

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,24 +1,9 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-#include <stdlib.h>
+#include <stddef.h>
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
 int _putchar(char c);
-
-/**
- * print_name - prints a name using a given function
- * @name: The name to print
- * @f: A function pointer to the function that prints the name
- */
-void print_name(char *name, void (*f)(char *));
-
-void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif /* FUNCTION_POINTERS_H */
