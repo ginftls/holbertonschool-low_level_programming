20351419f605ec143d9cbbd1b07d7e9f64602d0f
ginftls <scdr1905@gmail.com>
2024-12-04 10:25:56 -0500

    07

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,56 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: double pointer to the head of the list
+ * @idx: index where the new node should be added
+ * @n: value to be stored in the new node
+ *
+ * Return: address of the new node, or NULL if it failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node, *current;
+	unsigned int count = 0;
+
+	/* Special case: inserting at the beginning */
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	/* If list is NULL and index is not 0, return NULL */
+	if (h == NULL || *h == NULL)
+		return (NULL);
+
+	current = *h;
+
+	/* Traverse to the node before the insertion point */
+	while (current != NULL && count < idx - 1)
+	{
+		current = current->next;
+		count++;
+	}
+
+	/* Check if index is out of bounds */
+	if (current == NULL)
+		return (NULL);
+
+	/* Special case: inserting at the end */
+	if (current->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	/* Create new node */
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+
+	/* Adjust pointers to insert the new node */
+	new_node->next = current->next;
+	new_node->prev = current;
+	current->next->prev = new_node;
+	current->next = new_node;
+
+	return (new_node);
+}
--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,7 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
+#include <stdlib.h>
 
 /**
  * struct dlistint_s - doubly linked list
@@ -21,9 +21,8 @@ typedef struct dlistint_s
 /* Function prototypes */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 size_t print_dlistint(const dlistint_t *h);
 void free_dlistint(dlistint_t *head);
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
-int sum_dlistint(dlistint_t *head);
 
 #endif /* LISTS_H */
