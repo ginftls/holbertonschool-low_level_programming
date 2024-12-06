4ddc63bd40939081785f0ba6d92520363682342e
ginftls <scdr1905@gmail.com>
2024-12-06 12:03:24 -0500

    03

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -18,15 +18,14 @@ int main(int argc, char *argv[])
 	int fd_from, fd_to;
 	ssize_t bytes_read, bytes_written;
 	char buffer[BUFFER_SIZE];
+	mode_t file_permissions = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
-	/* Check correct number of arguments */
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	/* Open source file */
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
@@ -34,8 +33,7 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	/* Open destination file with proper permissions */
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_permissions);
 	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
@@ -43,11 +41,10 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	/* Read and write in chunks */
 	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written != bytes_read)
+		if (bytes_written == -1 || bytes_written != bytes_read)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(fd_from);
@@ -56,7 +53,6 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	/* Check for read error */
 	if (bytes_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
@@ -65,7 +61,6 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	/* Close file descriptors */
 	if (close(fd_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
