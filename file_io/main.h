97e77aee6864f59a00f827c80c12d30cb3c524d9
ginftls <scdr1905@gmail.com>
2024-12-06 11:55:32 -0500

    03

--- file_io/main.h
+++ file_io/main.h
@@ -1,13 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Standard C library headers */
 #include <unistd.h>
 #include <fcntl.h>
 
-/* Function prototype for read_textfile */
+/* Prototype for _putchar */
 int _putchar(char c);
-ssize_t read_textfile(const char *filename, size_t letters);
-int create_file(const char *filename, char *text_content);
-int append_text_to_file(const char *filename, char *text_content);
 
 #endif /* MAIN_H */
