3e4a556123409d7464c84a631ed49afa843b142f
ginftls <scdr1905@gmail.com>
2024-12-06 06:13:51 -0500

    00

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -2,30 +2,19 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
-
-/**
- * read_textfile - Reads a text file and prints it to POSIX stdout
- * @filename: Name of the file to read
- * @letters: Number of letters to read and print
- *
- * Return: Actual number of letters read and printed
- */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t bytes_read, bytes_written;
 	char *buffer;
 
-	/* Check if filename is NULL */
 	if (filename == NULL)
 		return (0);
 
-	/* Open the file with read-only permissions */
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
-	/* Allocate memory for buffer */
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
@@ -33,7 +22,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	/* Read from the file */
 	bytes_read = read(fd, buffer, letters);
 	if (bytes_read == -1)
 	{
@@ -41,15 +29,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-
-	/* Write to standard output */
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-
-	/* Clean up */
 	free(buffer);
 	close(fd);
 
-	/* Check if write was successful */
 	if (bytes_written != bytes_read)
 		return (0);
 
